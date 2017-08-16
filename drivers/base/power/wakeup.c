4d7bdacf50856a4b861c1c74a51d89d2d0fc08ad
Unundra <carlosfhserra@gmail.com>
2017-08-16 19:01:23 +0000

    Block wlan_oob_irq_wake wakelock

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -25,6 +25,8 @@ cdstatic bool enable_timerfd_ws = true;
 module_param(enable_timerfd_ws, bool, 0644);
 static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
+static bool enable_wlan_oob_irq_wake_ws = true;
+module_param(enable_wlan_oob_irq_wake_ws, bool, 0644);
 
 #include "power.h"
 
@@ -468,7 +470,9 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 			(!enable_timerfd_ws &&
                        		!strncmp(ws->name, "[timerfd]", wslen)) ||
 			(!enable_netlink_ws &&
-                        	!strncmp(ws->name, "NETLINK", wslen))) {
+                        	!strncmp(ws->name, "NETLINK", wslen))
+			(!enable_wlan_oob_irq_wake_ws &&
+                        	!strncmp(ws->name, "wlan_oob_irq_wake", wslen))) {
 			if (ws->active) {
 				wakeup_source_deactivate(ws);
 				pr_info("forcefully deactivate wakeup source: %s\n",
